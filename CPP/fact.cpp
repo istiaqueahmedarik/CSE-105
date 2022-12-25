d3eda1cde71641b6290fc80f15c75acdd44750f9
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2022-12-26 00:37:32 +0600

    26-12-2022

--- CPP/fact.cpp
+++ CPP/fact.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int fact(int n){
     if(n==1) return 1;
+    if(n==0) return 1;
     return n*fact(n-1);
 }
 int main()
--- test.c
+++ test.c
@@ -2,41 +2,17 @@
 int f(int n)
 {
     if (n == 1)
-        return 1;
-    else
     {
-        return n * n + f(n - 1);
-    }
-}
-int f1(int n)
-{
-    if (n == 1)
+        printf("1 ");
         return 1;
-    else
-    {
-        if (n % 2 == 0)
-        {
-            return f1(n - 1) - n;
-        }
-        else
-        {
-            return n + f1(n - 1);
-        }
-    }
+    };
+    f(n - 1);
+    printf("%d ", n);
 }
-int f2(int n)
-{
-    if (n == 1)
-        return 1;
-    if (n == 0)
-        return 0;
-    return f2(n - 1) + f2(n - 2);
-}
+
 int main()
 {
-    int n;
+    int r, n;
     scanf("%d", &n);
-    printf("%d\n", f(n));
-    printf("%d\n", f1(n));
-    printf("%d\n", f2(n));
+    r = f(n);
 }
