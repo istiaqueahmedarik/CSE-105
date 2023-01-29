91189854cf96be04e9628323f4f95d1ac64ae1c7
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2023-01-29 16:03:26 +0600

    29-1-23

--- CF/palindrome.cpp
+++ CF/palindrome.cpp
@@ -1,13 +1,21 @@
-#include<bits\stdc++.h>
-using namespace std; 
-bool pal(string s){
-    if(s.length()==1 || s.length()==0) return true;
-    if(s[0]==s[s.length()-1]){
-        return pal(s.substr(1,s.length()-2));
+#include <bits\stdc++.h>
+using namespace std;
+bool pal(string s)
+{
+    if (s.length() == 1 || s.length() == 0)
+        return true;
+    if (s[0] == s[s.length() - 1])
+    {
+        return pal(s.substr(1, s.length() - 2));
     }
     return false;
 }
 int main()
 {
-    cout<<pal("aba")<<endl;
+    string s;
+    cin >> s;
+    if (pal(s))
+        cout << "Yes";
+    else
+        cout << "No";
 }
--- test.cpp
+++ test.cpp
@@ -1,59 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> Merge(vector<int> arr)
+int MAX(int arr[], int n)
 {
-    if (arr.size() == 1)
-        return arr;
-    int mid = arr.size() / 2;
-    vector<int> left, right;
-    for (int i = 0; i < mid; i++)
-        left.push_back(arr[i]);
-    for (int i = mid; i < arr.size(); i++)
-        right.push_back(arr[i]);
-    vector<int> sorted_left = Merge(left);
-    vector<int> sorted_right = Merge(right);
-    vector<int> sorted;
-    int l = 0, r = 0;
-    for (int i = 0; i < arr.size(); i++)
-    {
-        if (l == sorted_left.size())
-        {
-
-            sorted.push_back(sorted_right[r]);
-            r++;
-        }
-        else if (r == sorted_right.size())
-        {
-
-            sorted.push_back(sorted_right[l]);
-            l++;
-        }
-        else if (sorted_left[l] < sorted_right[r])
-        {
-
-            sorted.push_back(sorted_right[l]);
-            l++;
-        }
-        else
-        {
-            sorted.push_back(sorted_right[r]);
-            r++;
-        }
-    }
-    return sorted;
+    if (n == 1)
+        return arr[0];
+    return max(arr[n - 1], MAX(arr, n - 1));
 }
 int main()
 {
     int n;
     cin >> n;
-    vector<int> arr(n);
+    int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    vector<int> ans = Merge(arr);
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << ans[i] << " ";
-    }
+    cout << MAX(arr, n) << "\n";
 }
